6d289df75b0e294819ef5d5e1c2c70fa8b65c824
Ade2002 <sikirusulaimon2002@gmail.com>
2022-06-06 23:48:26 +0100

    react hoook usecontext done and tutorial

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Header from "../components/Header";
 
 
-const Settings = () => <div className="charts">
-{/* <Header /> */}<p>hhh</p>
-</div>
+const Settings = () => (
+  <div className="charts">
+    {/* <Header /> */}
+    <p>hhh</p>
+    <SwitchImage />
+  </div>
+);
 
 export default Settings
