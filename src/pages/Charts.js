d9e2aa051c8b11f13fe2ed9bcf5537baa026fc4c
Ade2002 <sikirusulaimon2002@gmail.com>
2022-06-06 23:45:19 +0100

    react hoook usecontext done and tutorial

--- src/pages/Charts.js
+++ src/pages/Charts.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import Dummy from './Dummy'
+import SwitchImage from "./SwitchImage";
 const Charts = () =>
     <div className="charts">
         <Dummy params={'ade'}/>
